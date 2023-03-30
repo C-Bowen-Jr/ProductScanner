5ec7ec6dbd4ea8aaa35df680b6d6197a9882b7f4
C-Bowen-Jr <120433535+C-Bowen-Jr@users.noreply.github.com>
2023-03-30 15:11:31 -0400

    Add files via upload
    Posting current version to github.

---
+++ ProductScanner.py
@@ -0,0 +1,474 @@
+# -*- coding: utf-8 -*-
+
+# Inventory Server Product Scanner
+# Created 2019
+# GNU GPLv3
+
+import os
+from dotenv import load_dotenv
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import time, traceback, threading
+from datetime import datetime
+import select
+import sys
+import json
+
+# Version breakdown: Major change. Minor fix/change. Times code modified
+Version = "5.4.30"
+
+# Global variables
+RunState = "Run"
+DebugMode = False
+WeeklyReport = []
+ProductArray = []
+wkStock = 0
+wkSell = 0
+choice = -1
+
+# Classes
+class Product:
+  Title = "NULL"            # Name of Product
+  SKU = "SKU"               # SKU of Product
+  Stocked = 0               # Number in stock
+  Sold = 0                  # Number sold
+  Retired = False           # Retired don't show up on low stock
+  Released = "MM/DD/YYYY"   # Released date
+
+  # Init, takes ProductName and ..SKU to create a Product datapoint
+  def __init__(self, ProductName, ProductSKU, ProductStocked, ProductSold):
+    self.Title = ProductName
+    self.SKU = ProductSKU
+    self.Stocked = ProductStocked
+    self.Sold = ProductSold
+
+  def PrintProduct(self):
+    print (f"{self.Title} [In Stock: {self.Stocked} Sold: {self.Sold}]")
+
+  def GetManufactured(self):
+    return self.Stocked + self.Sold
+
+  def SetRetired(self, RetireState):
+    self.Retired = RetireState
+
+  def GetDictionaryForm(self):
+    DictForm = {"name": self.Title, "sku": self.SKU, "stock": self.Stocked, "sold": self.Sold, "released": self.Released,"retired": self.Retired}
+    return DictForm
+
+  def SetReleasedToPyDate(self, PyDatetimeDate):
+    self.Released = PyDatetimeDate.strftime('%m/%d/%Y')
+
+# Functions
+# Threading task delay    ------------------
+def every(delay, task):
+  next_time = time.time() + delay
+  while True:
+    time.sleep(max(0, next_time - time.time()))
+    try:
+      task()
+    except Exception:
+      traceback.print_exc()
+      # in production code you might want to have this instead of course:
+      # logger.exception("Problem while executing repetitive task.")
+    # skip tasks if we are behind schedule:
+    next_time += (time.time() - next_time) // delay * delay + delay
+
+# String Stripper, returns a subset of a string between 2 points
+def stringStripper(original, begining, ending):
+  if begining == None:
+    begining = 0 # 0 is the first index to the string
+  elif begining not in original:
+    return "Error: Begining not found"
+  else:
+    begining = original.find(begining)+1
+  if ending == None:
+    ending = len(original) # len() is the last index to the string
+  elif ending not in original:
+    return "Error: Ending not found"
+  else:
+    ending = original.find(ending)
+
+  return original[begining:ending]
+
+
+# Parse parscode variables to get html code
+def parsDecode(parscode):
+  global WeeklyReport, ProductArray, wkStock, wkSell
+  htmlreturn = ""
+  counter = 0
+  datebreak = 0
+  wreDate = ""
+  wreSKU = ""
+  wreQty = 0
+
+  if parscode == "LOGTABLE":
+    for WREntry in WeeklyReport:
+      if "&" in WREntry: # Expected code '&date[SKU]quantity'
+        wreDate = stringStripper(WREntry, "&", "[")
+        if datebreak != wreDate:
+          if datebreak != 0: # subsequent days need an end to the prior day's table
+            htmlreturn += "\n</table>"
+          htmlreturn += """\n<table style="color:white;"><caption><b>{0}</b></caption>
+    <tr>
+      <th>Product</th>
+      <th>Qty</th>
+    </tr>""".format(wreDate)
+          datebreak = wreDate # update a new datebreak point
+        wreSKU = stringStripper(WREntry, "[","]")
+        wreQty = int(stringStripper(WREntry, "]", None))
+        if wreQty > 0: # positive is green
+          wkStock += wreQty
+          htmlreturn += '<tr style="background-color: green;">'
+        elif wreQty < 0: # negative is red
+          wkSell -= wreQty
+          htmlreturn += '<tr style="background-color: red;">'
+        else:
+          htmlreturn += '<tr>'
+
+        htmlreturn +="""
+      <td>{0}</td>
+      <td>{1}</td>
+    </tr>""".format(wreSKU, wreQty)
+    # After all WREntry entries are parsed, add a final table break
+    htmlreturn += "\n</table>"
+
+  elif parscode == "INVENTORYTABLE":
+    htmlProdTemplate = """<tr{3}>
+      <td>{0}</td>
+      <td>{1}</td>
+      <td>{2}</td>
+    </tr>
+"""
+    for prod in ProductArray:
+      if not prod.Retired and prod.Stocked > 1:
+        htmlreturn += htmlProdTemplate.format(prod.SKU, prod.Title, prod.Stocked, '')
+      elif not prod.Retired:
+        htmlreturn += htmlProdTemplate.format(prod.SKU, prod.Title, prod.Stocked,' style="background-color: red;"')
+
+  elif parscode == "TOTALSOLD":
+    for prod in ProductArray:
+      counter += prod.Sold
+    htmlreturn = "<td>{0}</td>".format(counter)
+  elif parscode == "TOTALMANUFACTURED":
+    for prod in ProductArray:
+      prod.GetManufactured()
+      counter += prod.GetManufactured()
+    htmlreturn = "<td>{0}</td>".format(counter)
+  elif parscode == "WEEKLYSOLD":
+    htmlreturn += "<td>{0}</td>".format(wkSell)
+  elif parscode == "WEEKLYMANUFACTURED":
+    htmlreturn += "<td>{0}</td>".format(wkStock)
+  elif parscode == "CURRENTLYSTOCKED":
+    for prod in ProductArray:
+      counter += prod.Stocked
+    htmlreturn = "<td>{0}</td>".format(counter)
+  elif parscode == "FINALREPORT":
+    for entry in WeeklyReport:
+      if entry.find("ERROR") > -1: # Error print in bold red
+        htmlreturn += '<b style="color:red;>"{0}</b><br>'.format(entry)
+      else:
+        htmlreturn += "{0}<br>".format(entry)
+  else:
+    return "<b>ERROR in parscode</b>"
+  return htmlreturn
+
+# WEEKLY REPORT FUNCTIONS
+def SendReportEmail():
+  global RunState, WeeklyReport, ProductArray, DebugMode
+
+  load_dotenv
+
+  # Create message container - the correct MIME type is multipart/alternative.
+  msg = MIMEMultipart('alternative')
+  msg['Subject'] = "Weekly Stock Report"
+  msg['To'] = os.envget("EMAIL_TO")
+  msg['From'] = os.envget("EMAIL_FROM")
+  MsgFromArray = ""
+  Stocks = []
+  EmailFormated = ""
+  html = open("ServerEmail_Template.html", "r")
+
+  server = smtplib.SMTP_SSL(os.envget("SMTP_NAME"), int(os.envget("SMTP_PORT")))
+  server.login(os.envget("EMAIL_FROM"), os.envget("EFROM_PASSWORD"))
+
+
+  # Build Weekly In/Out Report log
+  MsgFromArray = '\n'.join(WeeklyReport)
+
+  # Sort the list by SKU alphabeticaly and leave out retired products
+  ProductArray.sort(key=lambda x: x.SKU, reverse=False)
+  for Prods in ProductArray:
+    if not Prods.Retired:
+      Stocks.append(f"{Prods.Stocked}: {Prods.Title}")
+
+  for htmlLine in html:
+    # check if line contains parscode ie ##variable##
+    if "##" in htmlLine: #htmlLine.find("##") != -1:
+      stripParscode = htmlLine[htmlLine.find("##")+2:]
+      stripParscode = stripParscode[:stripParscode.find("##")]
+      EmailFormated += parsDecode(stripParscode)
+    else:
+      EmailFormated += htmlLine
+
+  # Record the MIME types of both parts - text/plain and text/html.
+  part1 = MIMEText("HTML code", 'plain')
+  part2 = MIMEText(EmailFormated, 'html')
+
+  # Attach parts into message container.
+  # According to RFC 2046, the last part of a multipart message, in this case
+  # the HTML message, is best and preferred.
+  msg.attach(part1)
+  msg.attach(part2)
+
+  if DebugMode:
+    #save email as html file instead
+    with open("EmailReport.html", "w") as f:
+      f.write(EmailFormated)
+  else:
+    #actually send email
+    server.sendmail(
+      os.envget("EMAIL_FROM"),
+      os.envget("EMAIL_TO"),
+      msg.as_string())
+    print (f"({datetime.now()}) Email sent to mail server")
+  html.close()
+  server.quit()
+
+def PerformUpdateCheck():
+  global RunState, WeeklyReport
+
+  # Check if "update.ver" exists, this is ProductScanner.py renamed temporarily and will replace the old version
+  if os.path.exists("update.ver"): # Update is available
+    updateVersionString = open("update.ver","r")
+    line = updateVersionString.readline().replace("\n","")
+    while "Version = " not in line:   # if Version code isn't here, check next line
+      line = updateVersionString.readline().replace("\n","")
+    RunState = "Update" # Upon completion of PS.py' main loop, main.py's main loop will get update OK
+    choice = "Update" # feed user input with update command because we cant kill input() loop
+
+    WeeklyReport.append(f"Update from {Version} to {line} - [{dtNow}]")
+    updateVersionString.close()
+
+def SundayNineAM():
+  global RunState, WeeklyReport, wkStock, wkSell, choice
+  dtNow = datetime.now()
+  if dtNow.hour == 9:#is 9 am
+    if dtNow.weekday() == 6: # is Sunday
+      # len() > 0 is something to report, (find."Update" AND len() > 1)
+      if WeeklyReport:       #the more pythonic way of len(WR) > 0
+        # send report and reset weekly detailings
+        SendReportEmail()
+        WeeklyReport = []
+        wkStock = 0
+        wkSell = 0
+
+      PerformUpdateCheck()
+
+# --------- Raw SAVE DATA LINES
+def ReadSaveData():
+  jFile = open("Products.json", "r")
+  ProductsJsonArray = json.load(jFile)
+  jFile.close()
+
+  return JsonToArray(ProductsJsonArray)
+
+def WriteSaveData(ProductsObjectArray):
+  global DebugMode
+  if DebugMode:  #dont save changes made durring debugmode
+    return
+
+  jFile = open("Products.json", "w")
+
+  jFile.seek(0)
+  jFile.write(json.dumps(ProductsObjectArray, indent = 2))
+  jFile.truncate()
+  jFile.close()
+
+# /////////////////////////////////
+# --------- Program specific save data parser
+def JsonToArray(ProductsJsonArray):
+  # Take the json array and create an array of ProdObjects
+  tempProductObjectsArray = []
+  for jsonProd in ProductsJsonArray:
+    initProduct = Product(jsonProd["name"],jsonProd["sku"],jsonProd["stock"],jsonProd["sold"])
+    initProduct.SetRetired(jsonProd["retired"])
+    initProduct.Released = jsonProd["released"]
+    tempProductObjectsArray.append(initProduct)
+
+  return tempProductObjectsArray
+
+def ArrayToJson(ProductObjectArray):
+  # Take the array of ProdObjects and create a json array
+  tempProductsJsonArray = []
+  for prodObject in ProductObjectArray:
+    tempProductsJsonArray.append(prodObject.GetDictionaryForm())
+
+  return tempProductsJsonArray
+
+# ////////////////////////////////
+
+def ProdScanInit():
+  global RunState, DetailsMode, WeeklyReport, ProductArray, inputChoice
+  ProductArray = []
+
+  ProductArray = ReadSaveData()
+
+  ThreadTask = threading.Thread(target=lambda: every(60, SundayNineAM))
+  ThreadTask.daemon = True
+  ThreadTask.start()
+
+# Main Function
+def ProdScanMain():
+  global RunState, WeeklyReport, ProductArray, inputChoice
+  MenuOptions = ["Force Email","Force Update", "Clear Screen","Exit"]
+  WREcho = ""
+  WRETS = datetime.now()
+
+  inputChoice = -1
+  Opt = 1
+
+  print (f"---Inventory Server Product Scanner---  v({Version})")
+  if DebugMode:
+    print ("WARNING!! DEBUG MODE ACTIVE, NO SAVES WILL BE MADE!")
+  print ("Scan 'Q+[SKU](Product Name)#' to add a new product.")
+  print ("Scan '[SKU]*#' with +/- numbers to stock/sell, or use '0' for freebies")
+  for Option in MenuOptions:
+    print (str(Opt) + ": " + Option)
+    Opt += 1
+
+  #Test bed on main input loop
+  while True:
+    # Set inputChoice to a "no input" flag represented by -1
+    # Then set variables i Input, o Output and e Exception, only i will be used
+    inputChoice = -1
+    i, o, e = select.select( [sys.stdin], [], [], 1 )
+
+    # Set inputChoice from either user or from 9AM-> update thread
+    if (i):
+      inputChoice = sys.stdin.readline().strip()
+      sys.stdin.flush()
+    else:
+      # if there was no input, but choice has been changed to Update, then exit
+      # the loop
+      if inputChoice == "Update":
+        print ("Updating code found!")
+        return "Update" # Be sure to exit the loop
+
+    # Process the input if any as a menu choice or an QR action code
+    if inputChoice == -1:
+      pass
+    elif str(inputChoice).isdigit():
+      # offset inputChoice for a 0 based index of menu choices
+      inputChoice = int(inputChoice) - 1
+
+      # Menu option choices
+      # Force email with stock details (wont erase weekly report)
+      if MenuOptions[inputChoice] == "Force Email":
+        SendReportEmail()
+
+      elif MenuOptions[inputChoice] == "Clear Screen":
+        pass #print ("The screen is about to be cleared")
+      elif MenuOptions[inputChoice] == "Force Update":
+        print ("Checking...")
+        PerformUpdateCheck()
+      elif MenuOptions[inputChoice] == "Exit":
+        print ("Saving...")
+        WriteSaveData(ArrayToJson(ProductArray))
+        RunState = "Exit"
+
+      # Save and Exit
+      WriteSaveData(ArrayToJson(ProductArray))
+      return RunState
+    else:
+      #QR action code scanned
+      WeeklyReport.append(inputChoice) # Weekly Report, only add if not a menu item
+      WRETS = datetime.now()
+      FoundProd = False
+      # QuickAdd code found, parse
+      if "Q+[" in inputChoice:
+        tempSKU = stringStripper(inputChoice, "[","]")
+        tempTitle = stringStripper(inputChoice, "(",")")
+        tempMade = stringStripper(inputChoice, ")", None)
+        FoundProd = True #set if creating, technically true so no error later
+        WREcho = f"Creating new product! {tempTitle}[{tempSKU}] with an initial batch of {tempMade}"
+        print (WREcho)
+        WREcho = f"{WRETS.date()} {WRETS.hour}:{WRETS.minute} | {WREcho}"
+        WeeklyReport.append(WREcho) # Weekly Report
+        NewProduct = Product(tempTitle, tempSKU,int(tempMade),0)
+        NewProduct.SetReleasedToPyDate(WRETS.date())
+        ProductArray.append(NewProduct)
+        WriteSaveData(ArrayToJson(ProductArray))
+
+      # Check each product against the scanned barcode
+      if "retire" in inputChoice or "restore" in inputChoice or "inspect" in inputChoice:
+        for Prods in ProductArray:
+          # Check for the retire/rework code
+          if "retire:" in inputChoice and Prods.SKU == stringStripper(inputChoice, ":", None) :
+            # Get out of the loop now that we found the right one
+            FoundProd = True
+            Prods.SetRetired(True)
+            WeeklyReport.append(f"{Prods.Title} has been retired.")
+            break
+          elif "restore:" in inputChoice and Prods.SKU == stringStripper(inputChoice, ":", None) :
+            # Get out of the loop now that we found the right one
+            FoundProd = True
+            Prods.SetRetired(False)
+            WeeklyReport.append(f"{Prods.Title} has been taken out of retirement.")
+            break
+          elif "inspect:" in inputChoice and Prods.SKU == stringStripper(inputChoice, ":", None) :
+            # Get out of the loop now that we found the right one
+            FoundProd = True
+            print (f"Inspecting > {Prods.SKU}")
+            Prods.PrintProduct()
+            break
+
+      for Prods in ProductArray:
+        # Business as normal
+        SKUcount = 9999
+        try:
+          SKUcount = int(stringStripper(inputChoice, "*", None))#choice[choice.find("*")+1:])
+        except:
+          WREcho = f"'{inputChoice}' was scanned, but multiplier was not found"
+          FoundProd = False
+          break
+
+        # is this sku the choice sku and is it less than 0 (selling)
+        if Prods.SKU == stringStripper(inputChoice, None, "*") and SKUcount < 0:
+          Prods.Stocked += SKUcount # adding negative number subtracts stock
+          Prods.Sold -= SKUcount # subtracting negative number increases sold
+          Prods.PrintProduct()
+          FoundProd = True
+          WREcho = f"&{WRETS.date()}[{Prods.SKU}]{SKUcount}"
+          WeeklyReport.append(WREcho) # Weekly Report
+          WriteSaveData(ArrayToJson(ProductArray))
+          break
+
+        # is this sku the choice sku and is it more than 0 (restocking)
+        elif Prods.SKU == stringStripper(inputChoice, None, "*") and SKUcount > 0 and SKUcount < 100:
+          Prods.Stocked += SKUcount
+          Prods.PrintProduct()
+          FoundProd = True
+          WREcho = f"&{WRETS.date()}[{Prods.SKU}]{SKUcount}"
+          WeeklyReport.append(WREcho) # Weekly Report
+          WriteSaveData(ArrayToJson(ProductArray))
+          break
+
+        elif Prods.SKU == stringStripper(inputChoice, None, "*") and SKUcount == 0:
+          Prods.Stocked -= 1
+          Prods.PrintProduct()
+          FoundProd = True
+          WREcho = f"&{WRETS.date()}[{Prods.SKU}]{SKUcount}"
+          WeeklyReport.append(WREcho) # Weekly Report
+          WriteSaveData(ArrayToJson(ProductArray))
+          break
+
+      # No matching product, but also ignore the open/close statements
+      if not FoundProd:
+        WREcho = f" -{WRETS.date()} {WRETS.hour}:{WRETS.minute} | ERROR '{inputChoice}''"
+        WeeklyReport.append(WREcho) # Weekly Report
+
+
+# Notes:
+#
+
---
+++ readme.md
@@ -0,0 +1,134 @@
+## Inventory Server
+
+This is a simple inventory server to track products for small hobby businesses. The dataset it maintains is:
+- Product Name: a descritive title (ie. "Umbrella Corp Vinyl Sticker")
+- SKU: screaming snake case SKU (ie. UMBC_VINYL)
+- Sold: track the amount sold
+- Stock: track the amount currently in inventory
+- Created: date in which the product was added to the database
+- Retired: boolean flag for preserving data while excluding them from weekly reports
+
+## Notable service features
+
+- Maintain quantities of "In Stock" and "Sold" for various products.
+- Add new products and document their release or drop date.
+- Retire products from weekly emailed reports while maintaining otherwise normal behavior.
+- Update versions can be hotswapped in for headless server convience.
+
+## Implementation
+
+Deviation from this is possible, but might require modifing the code. Future versions might implement feature
+choices or other means to modify functionality without having to make changes to the source code. For now,
+this software is an open source version of my own use case.
+
+### Setup
+Personally, I run this on a raspberrypi, headless, and with a barcode scanner. You can certainly get away
+with running this on a main computer or something likewise. There is a feature here that only benefits from
+running 24/7 though. That being the weekly log report. Database changes take immediate effect on the save file
+though so it isn't a mandatory feature.
+
+Before running any code, the first thing to do is modify the file the file ```edit_first_env.txt```. Inside
+are already the fields that need to be filled out.
+- These are the fields listed
+
+Once these are filled out, save and exit. Rename that file to ```.env``` (no filename, just env extention
+type). This is to allow the source code to read the sensitive data that should not be saved into git or
+github.
+
+The next thing is ```Products.json```. This is the "database" file. There is an example product preloaded
+to give you an immediate hands on approach to initializing your database if that is easier. Delete this
+exact entry though as json has no comment-out-code option.
+
+### Running for the first time
+The ```main.py``` file is the code that will run the main loop of the function. This method is to allow for
+hot swapping updates. New versions of ```ProductScanner.py``` can be renamed as ```update.ver``` in the same
+directory.
+
+Once the program opens, there will be no preloaded products so you will have to enter them with the sequence
+```Q+[SKU](Title)#```. Let's look at a couple examples.
+
+    Q+[UMBC_VINYL](Umbrella Corp Vinyl Sticker)0
+    Q+[MAOWS](Mouse Cat Toy)5
+    Q+[SKOOMA_BRN](Do You Want Some Skooma Patch - Brown)2
+
+Each line should be entered one by one, multi-add is not a current feature. This though will create 3 different
+products in the json file used as the database. It will then initialize the Umbrella Corp sticker with 0 stock.
+The cat toy will have 5 immediately in stock, and 2 of the patches. All of these then also get date tagged with
+today's date. Date's don't have any direct use in this software, but can be useful to have for other business
+reasons elsewhere.
+
+### Stocking and Selling
+The main interaction with the software is to either stock or sell products. This is handled by a simple text
+command ```SKU*#```. Let's look at a couple examples.
+
+    UMBC_VINYL*2
+    MAOWS*-1
+    SKOOMA_BRN*0
+
+Again, each line should be entered one by one, multiline commands are not a current feature. The first example
+is a positive numbered SKU, so we are increasing the stocked quantity. This will add 2 to Stock on that SKU.
+There is no change to Sell. Next, we have -1 to sell 1 MAOWS. Stock will drop to 4 and Sell up to 1. For
+SKOOMA_BRN, I have added a feature that I personally use in the event of gifting. 0 specifically will reduce
+the Stock amount by 1, but wont increase Sell like a negative number would. This has a caviat of misrepresenting
+the true manufactured quantities, but does support tracking populatiry by sales. Meaning, if I make 20 of
+something that never sells, and I give them all away, it looks as though I never made them. Not great, but I am
+more concerned with not making it look like I successfully sold all 20 when truely no one intentionall bought
+them.
+
+### Retire, Restore, and Inspect
+These are helper functions to give you the information you need when you need it. Let's look at a couple examples.
+
+    retire:UMBC_VINYL
+    restore:MAOWS
+    inspect:SKOOMA_BRN
+
+Still, each line should be entered one by one, no multiline here yet. The first example will change the Umbrella
+Corp sticker to retired. That means it won't show up on weekly report tables. You can still sell or stock it.
+Next is restore. Rather than have the feature be ```retire:SKU``` to toggle retired on and off, retire and
+restore respectively set the retire flag to ```true``` or ```false```. Lastly is inspect. When you stock or sell
+things, by default, it will echo out sold and stock counts. Inspect is a way to easily poll that information
+without making such an action.
+
+### Menu features
+- Force Email: Prematurely sends current contents of actions to email.
+- Force Update: Prematurely runs update checker. CAUTION: This will drop floating data on action logs.
+- Clear Screen: Just clears the screen of all the built up screen echos.
+- Exit: Database modifications autosave, so exit will only drop weekly report data.
+
+## Reading the Weekly Report Email
+
+The weekly report email will be broken up into 4 sections.
+- Datewise ins and outs
+- Current inventory
+- Stats
+- Raw Log
+
+### Section 1
+Seperated by days, these tables come color coded as <span style="color:red">sold as red</span> and
+<span style="color:green">stocked as green</span>. Dates are written on top, SKUs are to the left, and quantity
+is to the right.
+
+### Section 2
+The current inventory gives 3 columns: SKU, Product Title, and Quantity. Default low stock is 2 or less and will
+highlight that row in red.
+
+### Section 3
+These are the stats as of the email being sent. This notes manufactured and sold in both totals and for the week.
+Also, current total of items in inventory.
+
+### Section 4
+This is more of a debugging section as this is the inputs with their timestamps. Errors here are supposed to be
+highlighted in red for convience, but due to a current problem yet to be fixed, it doesn't change the color back.
+The feature works, but not as intended.
+
+## Tie-in
+
+For best results in entering Sell/Stock codes, I have yet to release the companion app I personally made for
+this. It is currently extremely hard-coded to specification and not currently suitable for sharing. However,
+the concept, should you chose some likewise homemade solution, is to generate an image gallery of buttons
+that callback their associated SKU. Then with some counter buttons, increment or decrement the quantity part.
+
+## Compatability
+
+Current version uses a method for threading input with the main loop that doesn't work on Windows. Works fine
+with linux. Untested on Mac.
